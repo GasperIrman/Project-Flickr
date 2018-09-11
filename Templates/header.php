1c10926c705b94472d70bce271d6353e77758d86
Gašper <gasper.irman@gmail.com>
2018-09-11 10:07:39 +0200

    Index, header edit

--- Templates/header.php
+++ Templates/header.php
@@ -1,7 +1,14 @@
 <?php 	require_once('./Connection/database.php');?>
 <!DOCTYPE html>
 <html>
+	<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="./Templates/style.css">
+	</head>
 <body>
-	//top row
+	<nav>
+		<div class="nav">Link 1</div>
+		<div class="nav">Link 2</div>
+	</nav>
 </body>
 </html>
