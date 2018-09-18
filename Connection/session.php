209daae366d442683ca4fda2b8066b2eed362b2f
Gašper <gasper.irman@gmail.com>
2018-09-18 10:00:10 +0200

    Added redirect

--- Connection/session.php
+++ Connection/session.php
@@ -7,5 +7,8 @@
     if (!isset($_SESSION['user_id']) && !in_array($_SERVER['REQUEST_URI'], $allowed)) {
         header("Location: login.php");
     }
+    if(isset($_SESSION['user_id']) && $_SERVER['REQUEST_URI'] == "/Flickr/login.php") {
+        header("Location: index.php");
+    }
 
 ?>
