c1413b150462eacab825a23a2b95c5e180e9b4d4
Gašper <gasper.irman@gmail.com>
2018-09-24 13:45:44 +0200

    Image upload edit

--- upload.php
+++ upload.php
@@ -5,7 +5,11 @@
 <form action="PHP/uploadImg.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="file" id="fileToUpload"><br>
+    <input type="text" name="tags"><br>
+    <input type="text" name="title"><br>
+    <input type="text" name="description"><br>
     <input type="submit" value="Upload Image" name="submit">
+
 </form>
 
 </body>
