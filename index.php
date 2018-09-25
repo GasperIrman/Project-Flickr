7e441b23ca04c6f50b2fa7ab1de31b2a948a1375
Gašper <gasper.irman@gmail.com>
2018-09-25 08:22:53 +0200

    Index and login edit

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 
   <body>
     <?php 
+    //SELECT objave vseh, ki jih sledimo
     $query = "SELECT * FROM posts WHERE user_id IN (SELECT user_id FROM follows WHERE follower_id = ?) ORDER BY date DESC";
     if(isset($_GET['s'])){
     	$search = $_GET['s'];
     	if($search[0] == '#')
     	{
+
+            //SELECT objav z določenimi tagi
     		$query = "SELECT * FROM posts p INNER JOIN posts_tags pt ON pt.post_id = p.id INNER JOIN tags t ON t.id = pt.tag_id WHERE tag = '".substr($search, 1)."'";
     		    $stmt = $pdo->prepare($query);
     			$stmt->execute();
@@ -22,6 +25,7 @@
     	}
     	else
     	{
+            //SELECT upodabnikov iz searcha
     		$query = "SELECT * FROM users WHERE name LIKE '%".$search."%' OR 
     		surname LIKE '%".$search."%'";
     		$stmt = $pdo->prepare($query);
@@ -36,19 +40,23 @@
 
     	}
     }
+    //Če $_GET['s'] ni nastavljen gre po default
     else
     {
     	$stmt = $pdo->prepare($query);
     	$stmt->execute([$_SESSION['user_id']]);
 
     	$line = $stmt->fetch();
+        if(!empty($line))
+        {
 	    for($i = 0; $i<$stmt->rowCount(); $i++)
 	    {
 	    	echo '<div style="text-align: center; margin-top: 50px; margin-botom: 50px"><h3 style="text-align: left">'.$line['title'].'</h3><img width="600" src="'.$line['url'].'" alt="Image missing">
-	    	<p style="text-align: left">Description: '.$line['description'].'</p>
-	    	</div>';
+	    	<p style="text-align: left">Description: '.$line['description'].'</p>';
+	    	echo '</div>';
 	    	$line = $stmt->fetch();
 	    }
+    }
     }
     
 
