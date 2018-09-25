8be2c9acc82e4c09e462b6466781dfc54f716651
Gašper <gasper.irman@gmail.com>
2018-09-25 08:23:21 +0200

    Added register

--- Connection/session.php
+++ Connection/session.php
@@ -2,7 +2,7 @@
     session_start();
 
     $allowed = ['/Flickr/login.php','/Flickr/PHP/login_check.php',
-                '/Flickr/register.php', '/Flickr/header.php', '/Flickr/PHP/googleLogin.php'];
+                '/Flickr/register.php', '/Flickr/header.php', '/Flickr/PHP/registerInsert.php'];
     if ((!isset($_SESSION['user_id']) && !in_array($_SERVER['REQUEST_URI'], $allowed)) && substr($_SERVER['REQUEST_URI'], 0 ,27) != "/Flickr/PHP/googleLogin.php") {
 		header("Location: login.php");
     }
--- PHP/registerInsert.php
+++ PHP/registerInsert.php
@@ -1,1 +1,19 @@
-// TODO register
+<?php
+require_once '../Connection/session.php';
+require_once '../Connection/database.php';
+ 
+
+if (isset($_POST['username'])&&isset($_POST['name'])&&isset($_POST['surname'])&&isset($_POST['email'])&&isset($_POST['password'])&&isset($_POST['birth']))
+{
+$usr = $_POST['username'];
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$birth  = $_POST['birth'];
+$query = "INSERT INTO users (username, name, surname, email, password, birthday) VALUES ('$usr', '$name', '$surname', '$email', '$password', '$birth')";
+$pdo->query($query);
+header("Location: ../index.php");
+}
+
+?>
