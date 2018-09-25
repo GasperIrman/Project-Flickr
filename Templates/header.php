c03ad0ff10b972ed5f99bb896f99a6d5dbe5f6aa
Gašper <gasper.irman@gmail.com>
2018-09-25 08:23:42 +0200

    Header edit

--- Templates/header.php
+++ Templates/header.php
@@ -6,25 +6,25 @@
 	<head>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="./Templates/styles.css"/>
+		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
 <body>
 		<nav id="menu">
 			<ul id="menulist">
-				<li><div class="nav py-2 d-none d-md-inline-block"><a href="index.php">Home</a></div></li>
-				 
-						<?php if(isset($_SESSION['user_id'])){echo '<li><div class="nav py-2 d-none d-md-inline-block"><a href="upload.php">Upload</a></div></li>';}
+				<li><div class="nav py-2 d-none d-md-inline-block"><a style="text-decoration: none" href="index.php">Home</a></div></li>
+						<?php if(isset($_SESSION['user_id'])){echo '<li><div class="nav py-2 d-none d-md-inline-block"><a style="text-decoration: none" href="upload.php">Upload</a></div></li>';}
 								?>
 								
 				
 				<li id="hello"><div class="nav py-2 d-none d-md-inline-block"> 
-					<?php if(isset($_SESSION['user_id'])){echo "Hello <a href=\"profile.php?username=".$_SESSION['username']."\">".$_SESSION['name']."</a>";}
-							else {echo '<a href="login.php">Log in</a>';} ?>
+					<?php if(isset($_SESSION['user_id'])){echo "Hello <a style=\"text-decoration: none\" href=\"profile.php?username=".$_SESSION['username']."\">".$_SESSION['name']."</a>";}
+							else {echo '<a style="text-decoration: none" href="login.php">Log in</a>';} ?>
 							</div></li>
 				<li id="logout"><div class="nav py-2 d-none d-md-inline-block">
-					<?php if(isset($_SESSION['user_id'])) echo'<a href="PHP/logout.php">Logout</a>'; else echo '<a href="register.php">Sign up</a>'?>
+					<?php if(isset($_SESSION['user_id'])) echo'<a style="text-decoration: none" href="PHP/logout.php">Logout</a>'; else echo '<a style="text-decoration: none" href="register.php">Sign up</a>'?>
 						</div></li>
-				<li id="logout"><div class="nav py-2 d-none d-md-inline-block">
-					<?php if(isset($_SESSION['user_id'])) echo'<form action="index.php"><input name="s" type="text" placeholder="Search.."></form>';?>
+				<li id="search" style="width: 20%"><div class="nav py-2 d-none d-md-inline-block">
+					<?php if(isset($_SESSION['user_id'])) echo'<form action="index.php"><input name="s" type="text" id="query" placeholder="Search.."></form>';?>
 						</div></li>
 
 			</ul>
